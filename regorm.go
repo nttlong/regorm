bc20fa7940b796ab985042e8f5ff9d081c31ad38
Nttlong <zugeliang2000@gmail.com>
2025-05-09 20:18:35 +0700

    ok

--- regorm.go
+++ regorm.go
@@ -1,11 +1,44 @@
 package regorm
 
 import (
-	"fmt"
+	"sync"
 
-	_ "github.com/go-sql-driver/mysql" // import mysql driver
+	"github.com/nttlong/regorm/dbconfig"
+	_ "github.com/nttlong/regorm/dbconfig"
+	"github.com/nttlong/regorm/dbconfig/dbconfig_mysql"
+	_ "github.com/nttlong/regorm/dbconfig/dbconfig_mysql"
+	"github.com/nttlong/regorm/dbconfig/dbconfig_postgres"
+	_ "github.com/nttlong/regorm/dbconfig/dbconfig_postgres"
 )
 
-func Test() {
-	fmt.Print("Hello, world!")
+var (
+	cachdBconfig      map[string]dbconfig.IDbConfig = make(map[string]dbconfig.IDbConfig)
+	lockCacheDbconfig                               = new(sync.RWMutex)
+)
+
+func New(driverName string) dbconfig.IDbConfig {
+	//check cache
+	lockCacheDbconfig.RLock()
+	ret, ok := cachdBconfig[driverName]
+	lockCacheDbconfig.RUnlock()
+	if ok {
+		return ret
+	}
+
+	//create new dbconfig
+	lockCacheDbconfig.Lock()
+	defer lockCacheDbconfig.Unlock()
+	if ret, ok = cachdBconfig[driverName]; ok {
+		return ret
+	}
+	if driverName == "postgres" {
+		ret = &dbconfig_postgres.PostgresDbConfig{}
+	} else if driverName == "mysql" {
+		ret = &dbconfig_mysql.MySqlDbConfig{}
+	} else {
+		panic("not support driver")
+	}
+	cachdBconfig[driverName] = ret
+	return ret
+
 }
